a6142493a7cbca604feba08ed518dbe4bf12266e
Rhino149 <cadenkaiser06@gmail.com>
2020-05-10 21:36:04 -0500

    make it better

--- commands/help.js
+++ commands/help.js
@@ -52,7 +52,7 @@ exports.conf = {
 
 exports.help = {
   name: "help",
-  category: "General",
+  category: "Info",
   description: "Displays all the available commands for your permission level.",
   usage: "help [command]"
 };
